b8736221787d7d91b54ec130ef04d07a5669334d
luizfelipetorres <fellipe116@gmail.com>
2022-09-14 14:54:10 -0300

    Update icon and themes

--- app/src/main/java/com/lftf/tiptime/MainActivity.kt
+++ app/src/main/java/com/lftf/tiptime/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.lftf.tiptime
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.lftf.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener {
+            val tip = calculateTip()
+            displayTip(tip)
+        }
+    }
+
+    private fun calculateTip(): Double {
+        val costString = binding.costOfService.text.toString()
+        val cost: Double? = costString.toDoubleOrNull()
+
+        if (cost == null || cost == 0.0) {
+            return 0.0
+        }
+
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        var tip: Double = tipPercentage * cost
+        if (binding.roundUpSwitch.isChecked)
+            tip = ceil(tip)
+
+        return tip
+    }
+
+    private fun displayTip(tip: Double) {
+        val formatedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formatedTip)
     }
 }
